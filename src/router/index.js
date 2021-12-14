498ba4e2dc27d96e33b8fdcd1bd03f073fc16a69
NgoQuocBao1010 <56999031+NgoQuocBao1010@users.noreply.github.com>
2021-12-14 23:50:16 +0700

    🆕 feat: update tab name

--- src/router/index.js
+++ src/router/index.js
@@ -7,22 +7,36 @@ const routes = [
         path: "/sort",
         name: "SortingVisualizer",
         component: SortingVisualizer,
+        meta: {
+            name: "Sort Visualizer",
+        },
     },
     {
         path: "/path",
         name: "Pathfinding",
         component: Pathfinding,
+        meta: {
+            name: "Pathfinding Visualizer",
+        },
     },
     // ** Non-exist path return to sort algo
     {
         path: "/:catchAll(.*)",
         redirect: { name: "SortingVisualizer" },
+        meta: {
+            name: "404 Not found",
+        },
     },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
+});
+
+router.beforeEach((to, from, next) => {
+    document.title = `${to.meta.name} | Algo Visulizer`;
+    return next();
 });
 
 export default router;
